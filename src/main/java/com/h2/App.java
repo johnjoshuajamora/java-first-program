bada93f3a8c88b32cc78e19f765e0859766339c3
johnjoshuajamora <129570592+johnjoshuajamora@users.noreply.github.com>
2023-04-01 12:38:16 +0800

    Task 1 - 10 Commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,12 +6,23 @@ package com.h2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i <= numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+    public
 }
