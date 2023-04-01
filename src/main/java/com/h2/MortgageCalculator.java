cac9ee6b3966a30b26330e39cded2061b3139125
John Joshua Jamora <johnjoshuajamora@gmail.com>
2023-04-01 13:41:02 +0800

    Update MortgageCalculator.java

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,15 +1,15 @@
 package com.h2;
 
 public class MortgageCalculator {
-    private long loanAmount;
+    private static long loanAmount;
     private static int termInYears;
-    private float annualRate;
+    private static float annualRate;
     private double monthlyPayment;
 
     public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
-        this.loanAmount = loanAmount;
-        this.termInYears = termInYears;
-        this.annualRate = annualRate;
+        MortgageCalculator.loanAmount = loanAmount;
+        MortgageCalculator.termInYears = termInYears;
+        MortgageCalculator.annualRate = annualRate;
     }
 
     private static int getNumberOfPayments() {
