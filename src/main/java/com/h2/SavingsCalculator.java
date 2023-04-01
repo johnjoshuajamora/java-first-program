7682c8f47d67a610f32d6463a38dba8fdf90df87
johnjoshuajamora <129570592+johnjoshuajamora@users.noreply.github.com>
2023-04-01 13:02:05 +0800

    Create SavingsCalculator.java

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,12 @@
+package com.h2;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+}
